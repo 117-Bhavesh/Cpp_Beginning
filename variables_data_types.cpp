b70f90eb7386d227750230cbaf10c2101f72f9a8
Bhavesh Singh <arvindbhvesh2004@gmail.com>
2024-08-26 23:27:41 +0530

    26th august test commit

--- variables_data_types.cpp
+++ variables_data_types.cpp
@@ -16,8 +16,20 @@ int main() {
     char grade = 'A';
     char initial = 'B';
 
-    std::cout << initial;
+    //boolean (a variable thats boolean has only two states, true (1) or false(0)) 
+    bool student = false;
+    bool power = true;
+    bool forsale = true;
+
+    //string (objects that represnt a sequence of text)
+    std::string name = "bro";
+    std::string day = "Friday";
+    std::string food = "pizza";
     
+
+
+    std::cout << power;
+
     return 0;
 
 }
