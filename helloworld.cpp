0230948a957557b258eafcac47a4f65c10078335
Ness <arvindbhavesh2004@email.com>
2024-08-26 18:12:30 +0530

    new line

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main(){
-    std::cout << "i like one piece!";
+    std::cout << "i like one piece!" << std::endl;
+    std::cout << "it's really good!" << std::endl;
     return 0;
 }
