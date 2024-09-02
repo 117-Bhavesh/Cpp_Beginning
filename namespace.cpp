37f67720c38ebd09722cbf4f2e778cbe321ac405
Bhavesh Singh <arvindbhavesh2004@gmail.com>
2024-09-03 01:11:53 +0530

    implying a namespace in local version

--- namespace.cpp
+++ namespace.cpp
@@ -8,15 +8,9 @@ namespace second {
 }
 
 int main() {
-    /* Namespace = provies a solution for preventing name conflicts in large
-    projects. Each entity needs a unique name. A namespace allows for indentically named 
-    entities as long as the namespaces are different.
-    */
-
-   int x = 0;
-    // the version of x in the second namespace, take that entity (second) and precede it before x.
-   std::cout << second::x;
-   //just outputing the value of x is known as local version
+    //if we are implying an entity then we don't need to prefix it to output it.
+    using namespace second;
+   std::cout << x;
 
     return 0;
 }
