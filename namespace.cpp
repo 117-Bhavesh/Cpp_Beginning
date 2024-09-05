507fac25e41be0fefb757bf151723161e56e2759
Bhavesh Singh <arvindbhavesh2004@gmail.com>
2024-09-05 08:06:30 +0530

    edited a comment

--- namespace.cpp
+++ namespace.cpp
@@ -4,10 +4,11 @@
 
 int main() {
     //if we are using namespace std we don't need to use the prefix std:: everywhere
-   //however the std namespace has hundreds of entities, so big chance of conflict
+   //however the "std namespace" has hundreds of entities, so big chance of conflict
+    //there is a safer alternative to using namespace std, it will narrow down the entities to what's in our use
 
-
-   using namespace std;
+   using std::cout;
+   using std::string;
 
    string name = "bro";
 
