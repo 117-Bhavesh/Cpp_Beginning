6a2149828df0080406ed5063f3389053736f9a0c
Bhavesh Singh <arvindbhavesh2004@gmail.com>
2024-09-05 09:31:51 +0530

    used namedef

---
+++ typedef_and_typealiases.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+//typedef std::vector<std::pair<std::string, int>> pairlist_t;
+typedef std::string text_t;
+//_t is conventionally used to represent the use of namedef
+
+int main() {
+
+     //pairlist_t pairlist;
+
+     text_t firstname = "Ness";
+
+     std::cout << firstname << '\n';
+
+     return 0;
+}
