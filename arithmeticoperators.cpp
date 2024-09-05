173886ec94d5ddbcc848e413c985d7fbf6dbbd2b
Bhavesh Singh <arvindbhavesh2004@gmail.com>
2024-09-06 00:38:15 +0530

    int and double variable for output

--- arithmeticoperators.cpp
+++ arithmeticoperators.cpp
@@ -3,7 +3,8 @@ int main()
 {
     //arithmetic operators = return the result of a specific arithmetic operation (+ - * /)
 
-    int students = 20;
+    //int students = 20;
+    double students = 20;
 
     //students = students + 5;
     //there is a shorter way to write this
@@ -20,9 +21,11 @@ int main()
     //students*=5;
 
     //students = students / 5;
-    students/=5;
+    //students/=5;
 
-
+    students/=3;
+    //since we used int variable the decimal portion didn't get output
+    //when int replaced with double variable, decimal output possible
     std::cout << students;
 
     return 0;
