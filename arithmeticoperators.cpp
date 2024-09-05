42fa6e743de4345c58c2ebd0b35a30f281e4d110
Bhavesh Singh <arvindbhavesh2004@gmail.com>
2024-09-05 22:44:08 +0530

    addition and increament operator

---
+++ arithmeticoperators.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+int main()
+{
+    //arithmetic operators = return the result of a specific arithmetic operation (+ - * /)
+
+    int students = 20;
+
+    //students = students + 5;
+    //there is a shorter way to write this
+    //students+=5;
+    //if we only have to add 1 we can use the increament operator
+    students++; //++ is common in many loops
+
+    std::cout << students;
+
+    return 0;
+}
