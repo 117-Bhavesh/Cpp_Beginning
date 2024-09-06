fa40174a54123d0477c14f3a9961ce0c9761fabd
Bhavesh Singh <arvindbhavesh2004@gmail.com>
2024-09-06 22:32:34 +0530

    takes input from user but without space

---
+++ userinput.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+//cout << (insertion operator)
+// cin >> (extraction operator)
+//>> is called extraction operator and << is called insertion operator
+
+int main()
+{
+    std::string name;
+    int age;
+
+    std::cout << "what's your name?: ";
+    std::cin >> name;
+
+    std::cout << "What's your age?: ";
+    std::cin >> age;
+
+    std::cout << "Hello " << name << "\n";
+    std::cout << "You are " << age << " years old";
+    
+    return 0;
+}
