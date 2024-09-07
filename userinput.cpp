55c4f12affc91651efd5bdb182ad177b591d0a65
Bhavesh Singh <arvindbhavesh2004@gmail.com>
2024-09-07 22:05:54 +0530

    userinput with white space fix

--- userinput.cpp
+++ userinput.cpp
@@ -9,17 +9,22 @@ int main()
     std::string name;
     int age;
 
+    std::cout << "What's your age?: ";
+    std::cin >> age;
+
     std::cout << "what's your name?: ";
     //but te string variable doesn't take 'space' as input
     //std::cin >> name;
     //but this can be fixed by using getline
     //if you need to accept any string that includes any spaces, use getline function
-    std::getline(std::cin, name);
+    std::getline(std::cin >> std::ws, name);
+    //this space problem for \n can be fixed by adding std::ws
+    //this function will remove any 'white spaces' before any user input
     
-
-    std::cout << "What's your age?: ";
-    std::cin >> age;
-
+    /*
+    but there is a problem if we enter the cin function followed by getline function,
+     it takes /n new line function as input.
+    */
     std::cout << "Hello " << name << "\n";
     std::cout << "You are " << age << " years old";
     
