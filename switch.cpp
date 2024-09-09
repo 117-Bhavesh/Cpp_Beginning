c53c0ba72b80376a3489c402854d4f9b99df1dd5
Bhavesh Singh <arvindbhavesh2004@gmail.com>
2024-09-10 00:13:45 +0530

    switch

--- switch.cpp
+++ switch.cpp
@@ -8,7 +8,48 @@ int main() {
     std::cout << "enter the month (1-12): ";
     std::cin >> month;
 
-    if(month == 1){
+    switch(month){
+        case 1:
+             std::cout << "it is January";
+            break;
+        case 2:
+            std::cout << "it is February";
+            break;
+        case 3:
+            std::cout << "it is March";
+            break;
+        case 4:
+            std::cout << "it is April";
+            break;
+        case 5:
+             std::cout << "it is May";
+            break;
+        case 6:
+            std::cout << "it is June";
+            break;
+        case 7:
+            std::cout << "it is July";
+            break;
+        case 8:
+            std::cout << "it is August";
+            break;
+        case 9:
+             std::cout << "it is September";
+            break;
+        case 10:
+            std::cout << "it is October";
+            break;
+        case 11:
+            std::cout << "it is November";
+            break;
+        case 12:
+            std::cout << "it is December";
+            break;
+        default:
+            std::cout << "Please enter only numbers (1-12)"; 
+    }
+
+/*  if(month == 1){
         std::cout << "it is January";
     }
     else if(month == 2){
@@ -44,6 +85,7 @@ int main() {
     else if(month == 12){
         std::cout << "it is December";
     }
+*/
 
 
     return 0;
