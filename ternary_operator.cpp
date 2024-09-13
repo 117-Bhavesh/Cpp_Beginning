673129cbce339177651d9d6cf6b1f512ad59367a
Bhavesh Singh <arvindbhavesh2004@gmail.com>
2024-09-13 15:38:39 +0530

    ternary operator complete

--- ternary_operator.cpp
+++ ternary_operator.cpp
@@ -4,14 +4,19 @@ int main()
 {
     // ternary operator ?: - replacement to an if/else statement
     //condition ? expression1 : expression2;
+    //std::cout << (condition ? expresion1 : expression2);
 
     //int grade = 50;
     //grade >= 60 ? std::cout << "you pass!": std::cout << "you fail!";
 
-    int number = 10;
+    //int number = 10;
     /* number % 2 means number 'modulus' 2 , it gives the remainder of any division */
-    number % 2 == 1 ? std::cout << "ODD" : std::cout << "EVEN";
+    //number % 2 == 1 ? std::cout << "ODD" : std::cout << "EVEN";
     
+    bool hungry = true;
+
+    //hungry ? std::cout << "you are hungry" : std::cout << "you are full";
+    std::cout << (hungry ? "you are hungry" : "you are full");
 
     return 0;
 }
