3df7e10108537505bc3ed338d12bde1449da2f7a
Bhavesh Singh <arvindbhavesh2004@gmail.com>
2024-09-18 00:04:22 +0530

    putting condition on while loop

--- while_loops.cpp
+++ while_loops.cpp
@@ -10,14 +10,20 @@ int main(){
         std::getline(std::cin, name);
     }*/
 
-   //replacing 'if' funtion with 'while' to execute the true condition poetentially infinite amount of time
-   while(name.empty()){
+    //replacing 'if' funtion with 'while' to execute the true condition poetentially infinite amount of time
+    /*while(name.empty()){
         std::cout << "enter your name: ";
         std::getline(std::cin, name);
-    }
+    }*/
     //this will keep executing the program until we enter something
     //there is no way to exit this while loop
 
+    //we do want some way to exit this while loop
+    //so we write a condition we won't be able to change at all
+    while(1==1){
+        std::cout << "HELP I AM STUCK IN AN INFINITE LOOP";
+    }
+    //use case of while loops: when you have to force a user to do something to proceed further
     std::cout << "hello " << name;
     return 0;
 }
