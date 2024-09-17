1be2731a7114ee013caef598ec44590ccec19ed7
Bhavesh Singh <arvindbhavesh2004@gmail.com>
2024-09-17 23:57:44 +0530

    to check if name empty

--- while_loops.cpp
+++ while_loops.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 
-int main() {
-//while loops are much like if statements
+int main(){
+    //a while loop is much like an if statement except we can repeat some codes potentially and infinite amount of time
 
+    std::string name;
+    /*if(name.empty()){
+        std::cout << "enter your name: ";
+        //to check if user entered their name or not
+        std::getline(std::cin, name);
+    }*/
 
-return 0;
+   //replacing 'if' funtion with 'while' to execute the true condition poetentially infinite amount of time
+   while(name.empty()){
+        std::cout << "enter your name: ";
+        std::getline(std::cin, name);
+    }
+    //this will keep executing the program until we enter something
+    //there is no way to exit this while loop
+
+    std::cout << "hello " << name;
+    return 0;
 }
