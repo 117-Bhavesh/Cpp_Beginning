97a1d6175c3d5128252395101d38c63e9fa232fa
Bhavesh Singh <arvindbhavesh2004@gmail.com>
2024-09-18 22:59:56 +0530

    other for loop example, countdown

--- for_loops2.cpp
+++ for_loops2.cpp
@@ -2,9 +2,21 @@
 
 int main(){
     //another example of for loop: counting before HAPPY BNEW YEAR message
-    for(int i=1; i<=10; i++){
+/*    for(int i=1; i<=10; i++){
         std::cout << i << '\n';
     }
     std::cout << "HAPPY NEW YEAR\n";
+*/ 
+    //we can even skip iterations during increament or decrement
+    //for(int i=1; i<=10; i+=2)
+    //for(int i=10; i>=0; i--) /*countdown*/
+        for(int i=10; i>=0; i-=2){
+        std::cout << i << '\n';
+    }
+    std::cout << "HAPPY NEW YEAR\n";
+    //for loops are better in situations where you only need to repeat code a certain amount of time
+
+
+    
     return 0;
 }
