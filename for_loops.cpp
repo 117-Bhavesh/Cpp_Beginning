6faa855acd410bbce319d5f34ffbef92741b3df0
Bhavesh Singh <arvindbhavesh2004@gmail.com>
2024-09-18 22:50:54 +0530

    simple for loop

---
+++ for_loops.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(){
+    //a for loop executes a block of code a specified amount of times
+    //within for loops there are upto 3 statements that we can add
+    //first is that we can create an index 
+    //'index' can be shortened to just 'i'
+    for(int i=1;/*starting condition*/ 
+    i<=3;/*stopping condition: it will continue as long as i is <=3*/ 
+    i++/*we can increament or decrement our counter i.e. index*/){
+        std::cout << "HAPPY NEW YEAR\n";
+    }
+    //happy new year will be repated thrice
+
+
+    return 0;
+}
