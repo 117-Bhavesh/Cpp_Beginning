4968588975436a4623c85d9795b8ded277f302cb
Bhavesh Singh <arvindbhavesh2004@gmail.com>
2024-09-18 22:57:34 +0530

    count up with for loop

---
+++ for_loops2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+    //another example of for loop: counting before HAPPY BNEW YEAR message
+    for(int i=1; i<=10; i++){
+        std::cout << i << '\n';
+    }
+    std::cout << "HAPPY NEW YEAR\n";
+    return 0;
+}
