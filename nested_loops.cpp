6c1ef56747117892e6bddeb7f10960c3e58e5c12
Bhavesh Singh <arvindbhavesh2004@gmail.com>
2024-09-30 15:38:14 +0530

    nested loop example

--- nested_loops.cpp
+++ nested_loops.cpp
@@ -8,11 +8,26 @@ int main(){
         }
     }
 */
-    for(int i = 1; i <= 3; i++){
-        for(int j = 1; j <= 10; j++){
-        std::cout << j << ' ';
+    int rows;
+    int columns;
+    char symbol;
+
+    std::cout << "how many rows?: ";
+    std::cin >> rows;
+
+    std::cout << "how many columns?: ";
+    std::cin >> columns;
+
+    std::cout << "enter a symbol to use: ";
+    std::cin >> symbol;
+
+    for(int i = 1; i <= rows; i++){
+        //common naming convention for the inner loop is to use j as the counting index
+        for(int j = 1; j <= columns; j++){
+        std::cout << symbol;
         }
         std::cout << '\n';
+        //everytime we will exist the loop we will create a new line character
     }
 
 
