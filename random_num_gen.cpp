1a8e0e970fdd03f372e709e5aa8654ec8978f426
Bhavesh Singh <arvindbhavesh2004@gmail.com>
2024-09-30 23:09:38 +0530

    to roll three 6 face dice

--- random_num_gen.cpp
+++ random_num_gen.cpp
@@ -6,9 +6,12 @@ int main() {
     //within srand we need to imput a seed. commonly programmers use time as seed
     srand(time(NULL));
     //int num = rand(); will generate a random number between 0 to 32767
-    int num = (rand() % 6) + 1;  //plus to exclude the 0 in result
-    
-    std::cout << num;
+    int num1 = (rand() % 6) + 1;  //plus to exclude the 0 in result
+    int num2 = (rand() % 6) + 1;
+    int num3 = (rand() % 6) + 1;
 
+    std::cout << num1 << '\n';
+    std::cout << num2 << '\n';
+    std::cout << num3 << '\n';
     return 0;
 }
