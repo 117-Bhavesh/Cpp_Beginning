ad6ab66c8f00361f3f4008468e44da111cae7696
Bhavesh Singh <arvindbhavesh2004@gmail.com>
2024-11-17 23:22:47 +0530

    user input in array with quit functionality and closing the for loop when an element is empty

--- fill_user_input.cpp
+++ fill_user_input.cpp
@@ -4,17 +4,45 @@ int main(){
     std::string foods[5];//array are static data structures 
     //meaning their size cannot be changed once the program is being run
     int size = sizeof(foods)/sizeof(foods[0]);
-
+    std::string temp;
+    
     for(int i = 0; i < size; i++){
-        std::cout << "enter a food you like #" << i + 1 << ": ";
-        std::getline(std::cin, foods[i]);
+        std::cout << "enter a food you like or 'q' to quit #" << i + 1 << ": ";
+
+        //v e r s i o n 2 . 0
+
+        /*std::getline(std::cin, foods[i]);
+        if(foods[i] == "q")//but putting q in our foods array to stop the for loop just adds 'q' itself in the food list
+        {
+            break;
+        }*/
+
+       //therefore we will create a seperate variable for the quit feature itself
+       std::getline(std::cin, temp);
+       if(temp == "q"){
+            break;
+       }
+       else{
+            foods[i] = temp;
+       }
     }
+    
+    
 
     std::cout << "You like the following food:\n";
 
-    for(std::string food : foods){
-        std::cout << food << '\n';
+    //v e r s i o n 3 . 0
+    
+    //but this will also consider the emplty spaces as the elements of the for loop
+    //for(std::string food : foods){
+    //    std::cout << food << '\n';
+    //}
+
+    //in this for loop we will continue the for loop as long as the element isnt empty
+    for(int i = 0; !foods[i].empty(); i++){
+        std::cout << foods[i] << '\n';
     }
+
 
     return 0;
 }
